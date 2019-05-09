9fe3bf806cb4df116fedb8199e05f7f1a17f9819
Furkan Kurnaz <furkan.kurnaz@kgteknoloji.com>
2019-05-09 13:37:28 +0300

    creates outlet of animation view

--- UIView-Animation/ViewController.swift
+++ UIView-Animation/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var animationView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
