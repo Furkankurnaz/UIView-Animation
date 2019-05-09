bcb79f694a5f105a10c2cd92aefa868668827c62
Furkan Kurnaz <furkan.kurnaz@kgteknoloji.com>
2019-05-09 13:33:59 +0300

    Initial Commit

---
+++ UIView-Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIView-Animation
+//
+//  Created by Furkan Kurnaz on 9.05.2019.
+//  Copyright © 2019 Furkan Kurnaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
