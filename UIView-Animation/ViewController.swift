6977940e1a8fbde2da888adc3a6f7c417fe28968
Furkan Kurnaz <furkan.kurnaz@kgteknoloji.com>
2019-05-09 13:44:55 +0300

    adds an example usage of ViewAnimation class

--- UIView-Animation/ViewController.swift
+++ UIView-Animation/ViewController.swift
@@ -12,12 +12,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var animationView: UIView!
     
+    var viewAnimation: ViewAnimation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let screenBounds: CGRect = self.view.bounds
+        
+        viewAnimation = ViewAnimation(animationStyle: .inFromTopToBottom, view: animationView, bounds: screenBounds)
     }
 
     @IBAction func animateButtonTapped(_ sender: Any) {
-        
+        viewAnimation.animateView(duration: 0.5)
     }
     
 }
